b4c6bd189964b5de405e192a687e5af281f9f7b9
scul0405 <90521402+scul0405@users.noreply.github.com>
2023-02-28 10:41:58 +0700

    After chapter 4.8

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -46,7 +46,7 @@ func (m *SnippetModel) Get(id int) (*Snippet, error) {
 	err := row.Scan(&snippet.ID, &snippet.Title, &snippet.Content, &snippet.Created, &snippet.Expires)
 	/*
 		We can use shorthand single-record queries like this:
-		err := m.DB.QueryRow("SELECT ...", id).Scan(&snippet.ID, &snippet.Title, 
+		err := m.DB.QueryRow("SELECT ...", id).Scan(&snippet.ID, &snippet.Title,
 		&snippet.Content, &snippet.Created, &snippet.Expires)
 	*/
 	if err != nil {
@@ -63,5 +63,41 @@ func (m *SnippetModel) Get(id int) (*Snippet, error) {
 
 // This will return the 10 most recently created snippets.
 func (m *SnippetModel) Latest() ([]*Snippet, error) {
-	return nil, nil
+	stmt := `SELECT id, title, content, created, expires FROM snippets 
+	WHERE expires > NOW() ORDER BY id DESC LIMIT 10`
+
+	rows, err := m.DB.Query(context.Background(), stmt)
+	if err != nil {
+		return nil, err
+	}
+
+	// Closing a resultset with defer rows.Close() is critical. As long as 
+	// a resultset is open it will keep the underlying database connection open… so if
+	// something goes wrong in this method and the resultset isn’t closed, it can rapidly lead
+	// to all the connections in your pool being used up.
+	defer rows.Close()
+
+	// Empty slice to hold snippet structs
+	snippets := []*Snippet{}
+
+	for rows.Next() {
+		snippet := &Snippet{}
+
+		err = rows.Scan(&snippet.ID, &snippet.Title, &snippet.Content, &snippet.Created, &snippet.Expires)
+		if err != nil {
+			return nil, err
+		}
+		// Append it to the slice of snippets.
+		snippets = append(snippets, snippet)
+	}
+
+	// When the rows.Next() loop has finished we call rows.Err() to retrieve any
+	// error that was encountered during the iteration. It's important to
+	// call this - don't assume that a successful iteration was completed
+	// over the whole resultset.
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return snippets, nil
 }
