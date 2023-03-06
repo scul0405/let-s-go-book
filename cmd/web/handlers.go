6dad4d10b6dd953f5fd09d8a092aa45c8c258718
scul0405 <90521402+scul0405@users.noreply.github.com>
2023-03-06 21:20:31 +0700

    After chapter 5

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -21,10 +21,15 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Call the newTemplateData() helper to get a templateData struct containing
+	// the 'default' data (which for now is just the current year), and add the
+	// snippets slice to it.
+	data := app.newTemplateData(r)
+	data.Snippets = snippets
+	// Pass the data to the render() helper as normal.
+
 	// Use the new render helper.
-	app.render(w, http.StatusOK, "home.html", &templateData{
-		Snippets: snippets,
-	})
+	app.render(w, http.StatusOK, "home.html", data)
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
@@ -44,10 +49,11 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	data := app.newTemplateData(r)
+	data.Snippet = snippet
+
 	// Use the new render helper.
-	app.render(w, http.StatusOK, "view.html", &templateData{
-		Snippet: snippet,
-	})
+	app.render(w, http.StatusOK, "view.html", data)
 
 }
 
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,6 +3,7 @@ package main
 import (
 	"html/template"
 	"path/filepath"
+	"time"
 
 	"github.com/scul0405/let-s-go-book/internal/models"
 )
@@ -12,48 +13,49 @@ import (
 // At the moment it only contains one field, but we'll add more
 // to it as the build progresses.
 type templateData struct {
-	Snippet  *models.Snippet
-	Snippets []*models.Snippet
+	CurrentYear int
+	Snippet     *models.Snippet
+	Snippets    []*models.Snippet
+}
+
+// Create a humanDate function which returns a nicely formatted string
+// representation of a time.Time object.
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+// Initialize a template.FuncMap object and store it in a global variable. This is
+// essentially a string-keyed map which acts as a lookup between the names of our
+// custom template functions and the functions themselves.
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
-	// Initialize a new map to act as the cache.
 	cache := map[string]*template.Template{}
-	// Use the filepath.Glob() function to get a slice of all filepaths that
-	// match the pattern "./ui/html/pages/*.html". This will essentially gives
-	// us a slice of all the filepaths for our application 'page' templates
-	// like: [ui/html/pages/home.html ui/html/pages/view.html]
 	pages, err := filepath.Glob("./ui/html/pages/*.html")
 	if err != nil {
 		return nil, err
 	}
-	// Loop through the page filepaths one-by-one.
 	for _, page := range pages {
-		// Extract the file name (like 'home.html') from the full filepath
-		// and assign it to the name variable.
 		name := filepath.Base(page)
-
-		// Parse the base template file into a template set.
-		ts, err := template.ParseFiles("./ui/html/base.html")
+		// The template.FuncMap must be registered with the template set before you
+		// call the ParseFiles() method. This means we have to use template.New() to
+		// create an empty template set, use the Funcs() method to register the
+		// template.FuncMap, and then parse the file as normal.
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.html")
 		if err != nil {
 			return nil, err
 		}
-
-		// Call ParseGlob() *on this template set* to add any partials.
 		ts, err = ts.ParseGlob("./ui/html/partials/*.html")
 		if err != nil {
 			return nil, err
 		}
-
-		// Call ParseFiles() *on this template set* to add the page template.
 		ts, err = ts.ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
-
-		// Add the template set to the map as normal...
 		cache[name] = ts
 	}
-	// Return the map.
 	return cache, nil
 }
